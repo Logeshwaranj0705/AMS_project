a06c593aff3f7241f6cfa765175ad23de1080dc6
Logeshwaranj0705 <naru8sasu9344@gmail.com>
2024-10-21 20:48:57 +0530

    Update staff.py

--- AMS_codes/staff.py
+++ AMS_codes/staff.py
@@ -178,7 +178,7 @@ async def main(file_path, exam, year, sem):
         # Calculate arrear count
         count = 0
         subject = []  
-        for j in range(2, cols-1):
+        for j in range(3, cols-1):
             if int(data[i][j]) < 25:  # Assuming scores below 25 are considered arrears
                 subject.append(header[j] + '-' + str(data[i][j]))
                 count += 1
