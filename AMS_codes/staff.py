9e908c991d26b113d3f84de65ae63570eb3befe2
Logeshwaranj0705 <naru8sasu9344@gmail.com>
2024-10-22 19:46:24 +0530

    Update staff.py

--- AMS_codes/staff.py
+++ AMS_codes/staff.py
@@ -325,9 +325,9 @@ async def ESE_main(file_path, exam, year, sem):
         }
         if count >= 3:
             phone_number = "+91" + student_data['phone_number']
-            message = f"Dear {student_data['name']}, you have {count} Arrears (RA->Re_appear) in {exam.upper()} End semester Exam. Please take necessary action."
+            message = f"Dear {student_data['name']}, you have {count} Arrears (RA-'Re_appear') in {exam.upper()} End semester Exam. Please take necessary action."
             for subject_detail in subject:
-                message += f"\n{subject_detail}(RA)"
+                message += f"\n{subject_detail} - (RA)"
             tasks.append(send_sms_message(phone_number, message))
             cursor=cnx.cursor()
             qurey="USE 3_arrear_data"
