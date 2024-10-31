5d8822a5bc04a4e299968209503d3655880c3b47
Logeshwaranj0705 <naru8sasu9344@gmail.com>
2024-10-31 17:55:18 +0530

    Update staff.py

--- AMS_codes/staff.py
+++ AMS_codes/staff.py
@@ -218,7 +218,7 @@ async def main(file_path, exam, year, sem):
             phone_number = "+91" + student_data['phone_number']
             message = f"Dear {student_data['name']}, you have {count} Arrears in {exam.upper()}. Please take necessary action."
             for subject_detail in subject:
-                message += f"\n|    {subject_detail}      |"
+                message += f"\n{subject_detail}"
             tasks.append(send_sms_message(phone_number, message))
             cursor=cnx.cursor()
             qurey="USE 3_arrear_data"
@@ -327,7 +327,7 @@ async def ESE_main(file_path, exam, year, sem):
             phone_number = "+91" + student_data['phone_number']
             message = f"Dear {student_data['name']}, you have {count} Arrears in {exam.upper()} End-semester Exam. Please take necessary action (Note:RE-APPEAR(RA))."
             for subject_detail in subject:
-                message += f"\n|  {subject_detail}  | (RA) |"
+                message += f"\{subject_detail} (RA)"
             tasks.append(send_sms_message(phone_number, message))
             cursor=cnx.cursor()
             qurey="USE 3_arrear_data"
